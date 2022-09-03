75731d8e4b4a592b55c80b9a935b7814ccf59c3a
Faisal Nafees <mofaisal739@gmail.com>
2022-09-03 23:01:26 +0530

    Checking

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,6 +10,10 @@
                 <label for="exampleInputEmail1" class="form-label">Name</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" name="name" aria-describedby="emailHelp">
               </div>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="name" aria-describedby="emailHelp">
+              </div>
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
                   <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
